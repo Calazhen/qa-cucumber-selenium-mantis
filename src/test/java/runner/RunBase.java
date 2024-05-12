71d17f0cd0316faff9fff2b877721bc942610471
Calazhen <henriquecalazans.96@outlook.com>
2024-05-12 14:26:05 -0300

    Arrumando ambiente

--- src/test/java/runner/RunBase.java
+++ src/test/java/runner/RunBase.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class RunBase {
 
@@ -37,7 +38,9 @@ public class RunBase {
 
             case FIREFOX:
                 WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                driver = new FirefoxDriver(firefoxOptions);
                 driver.manage().window().maximize();
                 break;
 
